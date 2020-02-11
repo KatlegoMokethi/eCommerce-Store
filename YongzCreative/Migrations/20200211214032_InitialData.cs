bef07014a261f4cf6c94644a365601d1f376c530
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-02-11 23:48:40 +0200

    Add InitialData migrations

---
+++ YongzCreative/Migrations/20200211214032_InitialData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YongzCreative.Migrations
+{
+    public partial class InitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagePath",
+                table: "Products",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Description", "ImagePath", "Name", "Price" },
+                values: new object[,]
+                {
+                    { 1, "Black cap with a taste of red.", "~\\images\\Cap01.png", "YONGZ Cap", 275m },
+                    { 2, "Peach cap styled with maroon.", "~\\images\\Cap02.png", "YONGZ Cap", 1200m },
+                    { 3, "Black cap lightened up with white.", "~\\images\\Cap03.png", "YONGZ Cap", 1200m },
+                    { 4, "Maroon cap styled with white.", "~\\images\\Cap04.png", "YONGZ Cap", 1200m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4);
+
+            migrationBuilder.DropColumn(
+                name: "ImagePath",
+                table: "Products");
+        }
+    }
+}
