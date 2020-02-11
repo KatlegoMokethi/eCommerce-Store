afcbdc2361727db8c851b217fd155c969f1a031e
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-02-12 00:00:46 +0200

    Modify Home controller and its corresponding view.

--- YongzCreative/Controllers/HomeController.cs
+++ YongzCreative/Controllers/HomeController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using YongzCreative.Models;
 
 namespace YongzCreative.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IProductRepository _productRepository;
+
+        public HomeController(IProductRepository repo)
+        {
+            _productRepository = repo;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_productRepository.Products);
         }
     }
 }
