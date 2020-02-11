2202824a5d602150f3125466fbbf7a492380cc33
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-02-11 18:26:44 +0200

    Add Product, AppDbContext, IProductRepository and EFProductRepository files

---
+++ YongzCreative/Models/EFProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YongzCreative.Models
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private AppDbContext _context;        public EFProductRepository(AppDbContext context)        {            _context = context;        }        public IQueryable<Product> Products => _context.Products;
+    }
+}
