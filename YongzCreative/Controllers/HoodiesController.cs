f7cf78a8c2a7165276bd437a905c9d9386b8022d
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-04-22 03:31:48 +0200

    Add Hoodies controller

---
+++ YongzCreative/Controllers/HoodiesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using YongzCreative.Models;
+
+namespace YongzCreative.Controllers
+{
+    public class HoodiesController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public HoodiesController(IProductRepository repo)
+        {
+            _productRepository = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_productRepository.Products);
+        }
+    }
+}
