6556290d490500aba9bec74fbf23fb9d16c2fbe8
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-04-22 03:59:35 +0200

    Migrations to modify hoodie description

---
+++ YongzCreative/Migrations/20200422015903_ModifyHoodie.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YongzCreative.Migrations
+{
+    public partial class ModifyHoodie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "Description",
+                value: "A splashed hoodie that adds taste to your body.");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3,
+                column: "Description",
+                value: "A hoodie to rock your body.");
+        }
+    }
+}
