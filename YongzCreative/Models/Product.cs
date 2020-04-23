99774c0d10c147136b48fff26936b0e4e9b682db
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-04-24 00:11:57 +0200

    Add product status property

--- YongzCreative/Models/Product.cs
+++ YongzCreative/Models/Product.cs
@@ -6,10 +6,12 @@ using System.Threading.Tasks;
 namespace YongzCreative.Models
 {
     public enum Category { Cap, Hoodie, Sweater, T_Shirt, Sweatpants, Leggings };
+    public enum Status { Available, OutOfStock };
     public class Product
     {
         public int ProductId { get; set; }        public string Name { get; set; }        public string Description { get; set; }        public string ImagePath { get; set; }
         public decimal Price { get; set; }
         public Category category { get; set; }
+        public Status status { get; set; }
     }
 }
