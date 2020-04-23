8a856529b053153cadc527c46199d4a72ac97381
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-04-24 00:03:49 +0200

    Add product category

--- YongzCreative/Models/Product.cs
+++ YongzCreative/Models/Product.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 
 namespace YongzCreative.Models
 {
+    public enum Category { Cap, Hoodie, Sweater, T_Shirt, Sweatpants, Leggings };
     public class Product
     {
         public int ProductId { get; set; }        public string Name { get; set; }        public string Description { get; set; }        public string ImagePath { get; set; }
         public decimal Price { get; set; }
+        public Category category { get; set; }
     }
 }
