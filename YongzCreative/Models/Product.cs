571318d0c8ff5037f6397bc2ff3de187c21fe87a
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-04-23 23:48:11 +0200

    Revert "Modify product model"
    This reverts commit e5e125411241ed7e2b8493784560de1a948c5d4f.

--- YongzCreative/Models/Product.cs
+++ YongzCreative/Models/Product.cs
@@ -5,11 +5,9 @@ using System.Threading.Tasks;
 
 namespace YongzCreative.Models
 {
-    public enum Category { Hoodie, Sweater, T_Shirt, Cap, Sweatpants, Leggings};
     public class Product
     {
         public int ProductId { get; set; }        public string Name { get; set; }        public string Description { get; set; }        public string ImagePath { get; set; }
         public decimal Price { get; set; }
-        public Category Category { get; set; }
     }
 }
