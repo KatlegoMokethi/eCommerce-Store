fe213bce26887c8e8675cd3dda74bc591ed7188a
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-04-26 23:02:46 +0200

    Add Controller

---
+++ YongzCreative/Controllers/SweatpantsController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YongzCreative.Controllers
+{
+    public class SweatpantsController
+    {
+    }
+}
