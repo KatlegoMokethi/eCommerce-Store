94c446910bbe243a1d200ca08d7cda80ec5b8861
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-04-27 14:25:24 +0200

    Migrations for sweatpants

---
+++ YongzCreative/Migrations/20200427122441_AddSweatpants.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace YongzCreative.Migrations
+{
+    public partial class AddSweatpants : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "Description", "ImagePath", "Name", "Price", "category", "status" },
+                values: new object[,]
+                {
+                    { 35, "", "/images/sweatpants/Sweatpants00.png", "Grey Sweatpants", 300m, 4, 1 },
+                    { 36, "", "/images/sweatpants/Sweatpants01.png", "Red Sweatpants", 300m, 4, 0 },
+                    { 37, "", "/images/sweatpants/Sweatpants02.png", "Yellow Sweatpants", 300m, 4, 1 },
+                    { 38, "", "/images/sweatpants/Sweatpants03.png", "Grey Sweatpants", 300m, 4, 1 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 35);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 36);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 37);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 38);
+        }
+    }
+}
