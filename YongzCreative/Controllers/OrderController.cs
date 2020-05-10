41b0a32db3e446e72f13b5161d26f50db9ff8957
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-05-10 20:08:23 +0200

    Comment out implementation

--- YongzCreative/Controllers/OrderController.cs
+++ YongzCreative/Controllers/OrderController.cs
@@ -21,59 +21,59 @@ namespace YongzCreative.Controllers
         }
 
         //[Authorize(Roles = "Admins")]
-        public IActionResult List()
-        {
-            return View(_repository.Order.GetOrders().Where(o => !o.Shipped));
-        }
+        //public IActionResult List()
+        //{
+        //    return View(_repository.Order.GetOrders().Where(o => !o.Shipped));
+        //}
 
-        [HttpPost]
-        //[Authorize(Roles = "Admins")]
-        public IActionResult MarkShipped(int orderID)
-        {
-            Order order = _repository.Order.GetById(orderID);
-            if (order != null)
-            {
-                order.Shipped = true;
-                _repository.Order.Save();
-                TempData["Message"] = $"Order {orderID} has been shipped";
-            }
-            else
-            {
-                TempData["Message"] = $"Order {orderID} could not been shipped";
-            }
-            return RedirectToAction(nameof(List));
-        }
-        //[Authorize(Roles = "Customers")]
-        public IActionResult Checkout()
-        {
-            return View();
-        }
+        //[HttpPost]
+        ////[Authorize(Roles = "Admins")]
+        //public IActionResult MarkShipped(int orderID)
+        //{
+        //    Order order = _repository.Order.GetById(orderID);
+        //    if (order != null)
+        //    {
+        //        order.Shipped = true;
+        //        _repository.Order.Save();
+        //        TempData["Message"] = $"Order {orderID} has been shipped";
+        //    }
+        //    else
+        //    {
+        //        TempData["Message"] = $"Order {orderID} could not been shipped";
+        //    }
+        //    return RedirectToAction(nameof(List));
+        //}
+        ////[Authorize(Roles = "Customers")]
+        //public IActionResult Checkout()
+        //{
+        //    return View();
+        //}
 
-        [HttpPost]
-        //[Authorize(Roles = "Customers")]
-        public IActionResult Checkout(Order order)
-        {
-            if (_cart.Lines.Count() == 0)
-            {
-                ModelState.AddModelError("", "Sorry, your cart is empty!");
-            }
-            if (ModelState.IsValid)
-            {
-                order.Lines = _cart.Lines.ToArray();
-                _repository.Order.SaveOrder(order);
-                return RedirectToAction(nameof(Completed));
-            }
-            else
-            {
-                return View(order);
-            }
-        }
+        //[HttpPost]
+        ////[Authorize(Roles = "Customers")]
+        //public IActionResult Checkout(Order order)
+        //{
+        //    if (_cart.Lines.Count() == 0)
+        //    {
+        //        ModelState.AddModelError("", "Sorry, your cart is empty!");
+        //    }
+        //    if (ModelState.IsValid)
+        //    {
+        //        order.Lines = _cart.Lines.ToArray();
+        //        _repository.Order.SaveOrder(order);
+        //        return RedirectToAction(nameof(Completed));
+        //    }
+        //    else
+        //    {
+        //        return View(order);
+        //    }
+        //}
 
-        //[Authorize(Roles = "Customers")]
-        public IActionResult Completed()
-        {
-            _cart.Clear();
-            return View();
-        }
+        ////[Authorize(Roles = "Customers")]
+        //public IActionResult Completed()
+        //{
+        //    _cart.Clear();
+        //    return View();
+        //}
     }
 }
