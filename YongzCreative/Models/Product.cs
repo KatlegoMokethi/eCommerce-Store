6b58dde08b64c027099a6844f337bdb24395be82
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-05-19 23:42:14 +0200

    Add CategoryID and Category navigation property

--- YongzCreative/Models/Product.cs
+++ YongzCreative/Models/Product.cs
@@ -5,13 +5,14 @@ using System.Threading.Tasks;
 
 namespace YongzCreative.Models
 {
-    public enum Category { Cap, Hoodie, Sweater, T_Shirt, Sweatpants, Leggings };
     public enum Status { Available, OutOfStock };
     public class Product
     {
         public int ProductId { get; set; }        public string Name { get; set; }        public string Description { get; set; }        public string ImagePath { get; set; }
         public decimal Price { get; set; }
-        public Category category { get; set; }
         public Status status { get; set; }
+
+        public int CategoryID { get; set; }
+        public Category Category { get; set; }
     }
 }
