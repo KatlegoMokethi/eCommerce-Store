ab43a477413e06aefac1c1098f879f63927fc706
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-05-20 01:01:25 +0200

    Modify Categories query

--- YongzCreative/Models/EFProductRepository.cs
+++ YongzCreative/Models/EFProductRepository.cs
@@ -9,17 +9,17 @@ namespace YongzCreative.Models
     {
         private AppDbContext _context;        public EFProductRepository(AppDbContext context)        {            _context = context;        }        public IQueryable<Product> Products => _context.Products;
 
-        public IQueryable<Product> Hoodies => _context.Products.Where(p => p.category == Category.Hoodie);
+        public IQueryable<Product> Hoodies => _context.Products.Where(p => p.Category.CategoryName == "Hoodie");
 
-        public IQueryable<Product> Caps => _context.Products.Where(p => p.category == Category.Cap);
+        public IQueryable<Product> Caps => _context.Products.Where(p => p.Category.CategoryName == "Cap");
 
-        public IQueryable<Product> Sweaters => _context.Products.Where(p => p.category == Category.Sweater);
+        public IQueryable<Product> Sweaters => _context.Products.Where(p => p.Category.CategoryName == "Sweater");
 
-        public IQueryable<Product> Sweatpants => _context.Products.Where(p => p.category == Category.Sweatpants);
+        public IQueryable<Product> Sweatpants => _context.Products.Where(p => p.Category.CategoryName == "Sweatpants");
 
-        public IQueryable<Product> Leggings => _context.Products.Where(p => p.category == Category.Leggings);
+        public IQueryable<Product> Leggings => _context.Products.Where(p => p.Category.CategoryName == "Leggings");
 
-        public IQueryable<Product> TShirts => _context.Products.Where(p => p.category == Category.T_Shirt);
+        public IQueryable<Product> TShirts => _context.Products.Where(p => p.Category.CategoryName == "T Shirt");
 
         public Product GetById(int id) => _context.Products.Find(id);
     }
