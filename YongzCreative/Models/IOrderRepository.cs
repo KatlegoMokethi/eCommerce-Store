901169cd12dc4018aa6fff7220666d5632a26fe5
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-05-19 23:25:12 +0200

    Add IOrderRepository

---
+++ YongzCreative/Models/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YongzCreative.Models
+{
+    public interface IOrderRepository : IRepositoryBase<Order>
+    {
+        IQueryable<Order> GetOrders();
+        void SaveOrder(Order order);
+    }
+}
