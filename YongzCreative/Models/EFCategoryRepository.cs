5a5d982bf196e96fe61d2be4070feb565f4e9bdf
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-05-19 23:59:01 +0200

    Add EFCategoryRepository

---
+++ YongzCreative/Models/EFCategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YongzCreative.Models
+{
+    public class EFCategoryRepository : RepositoryBase<Category>, ICategoryRepository
+    {
+        public EFCategoryRepository(AppDbContext appDbContext)
+            : base(appDbContext)
+        {
+        }
+
+    }
+}
