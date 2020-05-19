132249abdd8e183d2aa772b10301244530be5c99
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-05-19 21:36:51 +0200

    Add User View Model

---
+++ YongzCreative/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace YongzCreative.Models.ViewModels
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; } = "/";
+    }
+
+    public class CreateModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class RoleEditModel
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<IdentityUser> Members { get; set; }
+        public IEnumerable<IdentityUser> NonMembers { get; set; }
+    }
+
+    public class RoleModificationModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public string RoleId { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
+    }
+}
