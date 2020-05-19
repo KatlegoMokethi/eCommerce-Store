f3357d3b4c646d91ed062b8de3d27bff1c0e4bf7
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-05-19 21:46:59 +0200

    Add Account Controller

---
+++ YongzCreative/Controllers/AccountController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using YongzCreative.Models.ViewModels;
+
+namespace YongzCreative.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager,
+        SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginModel
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel loginModel)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user =
+                await _userManager.FindByNameAsync(loginModel.Name);
+                if (user != null)
+                {
+                    await _signInManager.SignOutAsync();//Cancel any ewxisting sessions.
+                    var result = await _signInManager.PasswordSignInAsync(user,
+                    loginModel.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        return Redirect(loginModel?.ReturnUrl ?? "/Home/Index");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Invalid email or password");
+            return View(loginModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
