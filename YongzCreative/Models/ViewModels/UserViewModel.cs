ebaca704c2d93690d92aec954bb1e74062814124
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-05-21 12:43:02 +0200

    Update User View Model

--- YongzCreative/Models/ViewModels/UserViewModel.cs
+++ YongzCreative/Models/ViewModels/UserViewModel.cs
@@ -7,13 +7,34 @@ namespace YongzCreative.Models.ViewModels
     public class LoginModel
     {
         [Required]
-        public string Name { get; set; }
-
+        [UIHint("email")]
+        public string Email { get; set; }
         [Required]
         [UIHint("password")]
         public string Password { get; set; }
 
         public string ReturnUrl { get; set; } = "/";
+    }
+
+    public class RegisterModel
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "E-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords must match")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
     }
 
     public class CreateModel
