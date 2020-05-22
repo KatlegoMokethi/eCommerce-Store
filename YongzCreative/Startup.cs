911d0ec3221ac77b4efd6331e2d87986543fa0d1
Katlego Mokhethi <mokhethikatlego@gmail.com>
2020-05-23 01:02:44 +0200

    Update for getting connection strings

--- YongzCreative/Startup.cs
+++ YongzCreative/Startup.cs
@@ -26,26 +26,18 @@ namespace YongzCreative
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-
+            //For Production Environment
             //services.AddDbContext<AppDbContext>(options =>
             //options.UseSqlServer(configuration.GetConnectionString("AppDbConnection")));
             //services.AddDbContext<AppIdentityDbContext>(options =>
             //    options.UseSqlServer(configuration.GetConnectionString("AppIdentityDbConnection")));
 
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
-            {
-                services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("AppDbConnection")));
-                services.AddDbContext<AppIdentityDbContext>(options =>
-                    options.UseSqlServer(configuration.GetConnectionString("AppIdentityDbConnection")));
-            }
-            else
-            {
-                services.AddDbContext<AppDbContext>(options =>
-                    options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
-                services.AddDbContext<AppIdentityDbContext>(options =>
-                    options.UseSqlServer(configuration.GetConnectionString("IdentityConnection")));
-            }
+            //For Development Environment
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<AppIdentityDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("IdentityConnection")));
+
 
             services.AddIdentity<IdentityUser, IdentityRole>(opts =>
             {
